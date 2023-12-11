23d97bdba46922f82f8bcee4f629d416bbf7c39b
F1ZZ <TheKubonn@hotmail.com>
2023-12-11 18:54:39 +0100

    Added Item base class

---
+++ Source/SlashandBlood/SlashandBlood.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SlashandBlood.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SlashandBlood, "SlashandBlood" );
