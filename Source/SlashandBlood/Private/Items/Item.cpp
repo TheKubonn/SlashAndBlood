7a18e00e1eceefec8ecf7a6553b69e1d22df0d0d
F1ZZ <TheKubonn@hotmail.com>
2024-01-05 18:41:50 +0100

    Added sin to an actor and made variables visible in BP

--- Source/SlashandBlood/Private/Items/Item.cpp
+++ Source/SlashandBlood/Private/Items/Item.cpp
@@ -21,13 +21,15 @@ void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	// Movement rate in units of cm/s
-	float MovementRate = 50.f;
-	float RotationRate = 45.f;
+	RunningTime += DeltaTime;
+
+	float DeltaZ = Amplitude * FMath::Sin(RunningTime * TimeConstant); // period of sine wave = 2*pi/K
+	AddActorWorldOffset(FVector(0.f, 0.f, DeltaZ));
+
 
 	// MovementRate * DeltaTime (cm/s) * (s/frame) = (cm/frame)
-	AddActorWorldOffset(FVector(MovementRate * DeltaTime, 0.f, 0.f));
-	AddActorWorldRotation(FRotator(0.f, RotationRate * DeltaTime, 0.f));
+	//AddActorWorldOffset(FVector(MovementRate * DeltaTime, 0.f, 0.f));
+	//AddActorWorldRotation(FRotator(0.f, RotationRate * DeltaTime, 0.f));
 	DRAW_SPHERE_SingleFrame(GetActorLocation());
 	DRAW_VECTOR_SingleFrame(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f);
 }
--- Source/SlashandBlood/Public/Items/Item.h
+++ Source/SlashandBlood/Public/Items/Item.h
@@ -6,6 +6,13 @@
 #include "GameFramework/Actor.h"
 #include "Item.generated.h"
 
+/* EditDefaultsOnly - Pozwala na edycjê wartoœci w BP ale tylko w g³ównym BP, na scenie nie */
+/* EditInstanceOnly - Pozwala na edycjê wartoœci w BP na scenie, ale nie w g³ównym BP */
+/* EditAnywhere - Pozwala na edycjê wartoœci i w g³ównym BP, jak i równie¿ na scenie */
+/* VisibleDefaultsOnly - Pozwala widzieæ dan¹ wartoœæ w g³ównym BP, ale nie mo¿na jej zmieniæ w BP */
+/* VisibleInstanceOnly - Pozwala widzieæ dan¹ wartoœæ w BP na scenie, ale nie mo¿na jej zmieniæ */
+/* VisibleAnywhere - Pozwala widzieæ dan¹ wartoœæ w g³ównym BP jak i na scenie, ale nie mo¿na jej zmieniæ */
+
 UCLASS()
 class SLASHANDBLOOD_API AItem : public AActor
 {
@@ -17,5 +24,11 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
-
+private:
+	UPROPERTY(VisibleAnywhere)
+	float RunningTime;
+	UPROPERTY(EditAnywhere) 
+	float Amplitude = 0.25f;
+	UPROPERTY(EditAnywhere)
+	float TimeConstant = 5.f;
 };
