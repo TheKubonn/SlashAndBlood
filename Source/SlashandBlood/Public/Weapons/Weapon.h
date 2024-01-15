a6ea07e4b604343839221846512427ffe36504bf
F1ZZ <TheKubonn@hotmail.com>
2024-01-15 16:57:22 +0100

    Added sound to equipping weapon and fixes some bugs

--- Source/SlashandBlood/Public/Weapons/Weapon.h
+++ Source/SlashandBlood/Public/Weapons/Weapon.h
@@ -6,6 +6,8 @@
 #include "Items/Item.h"
 #include "Weapon.generated.h"
 
+class USoundBase;
+
 /**
  * 
  */
@@ -24,4 +26,8 @@ protected:
 
 	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
 
+private:
+
+	UPROPERTY(EditAnywhere, Category = "Weapon Properties")
+	USoundBase* EquipSound;
 };
